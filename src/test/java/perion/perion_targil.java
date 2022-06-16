43f1088a7e54de882943a1ea2c0452f1aee00d81
Admin <Admin@10.0.0.2>
2022-06-16 13:17:47 +0300

    test

--- src/test/java/perion/perion_targil.java
+++ src/test/java/perion/perion_targil.java
@@ -1,24 +1,34 @@
 package perion;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class perion_targil {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+		public void t1() {
+		   int arr1[]={5,1,4,3,6,7,2};
+		   int arr2[]={1,2,3,4,5,6,7};
+		   
+		   int arr3[]={1,2,3};
+		   int arr4[]={1,3,4};
+		    
+		    Arrays.sort(arr1);
+		    Arrays.sort(arr2);
+		    
+		    Arrays.sort(arr3);
+		    Arrays.sort(arr4);
+		   
+		    boolean returnVal1 = Arrays.equals(arr1,arr2);
+		    System.out.println("Array 1 and 2 : " + returnVal1);
+		    boolean returnVal2 = Arrays.equals(arr3,arr4);
+		    System.out.println("Array 3 and 4  : " + returnVal1);
 
-}
+		  
+		  
+		
+
+
+}}

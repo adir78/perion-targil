da670ef075c7d5014c8a4608c7c3ebedde6587d7
Admin <Admin@10.0.0.2>
2022-06-16 11:09:24 +0300

    first commit

---
+++ src/test/java/perion/perion_targil.java
@@ -0,0 +1,24 @@
+package perion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class perion_targil {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
